fa425a17dddfe32f94d51702017e991f4ca19484
mukesh-kk <mukeshkushwaha918@gmail.com>
2026-04-10 23:59:50 +0530

    add changes for ollama

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,52 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from langchain_ollama import OllamaLLM
 
-app=FastAPI()
-@app.get('/health')
-def get_data():
-    return {'message':'Hi I am working'}
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+llm = OllamaLLM(model="mistral")
+
+response = llm.invoke("Hello! your last trianing date ? ")
+print(response)
 
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+
+# app=FastAPI()
+# @app.get('/health')
+# def get_data():
+#     return {'message':'Hi I am working'}
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
