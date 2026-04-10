397f1b7ba4b46569349ebe30d1654273ecbe7fd6
mukesh-kk <mukeshkushwaha918@gmail.com>
2026-04-10 23:26:48 +0530

    initialize

---
+++ README.md
@@ -0,0 +1,1 @@
+## AI with Agents ,LLMS 
---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app=FastAPI()
+@app.get('/health')
+def get_data():
+    return {'message':'Hi I am working'}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
